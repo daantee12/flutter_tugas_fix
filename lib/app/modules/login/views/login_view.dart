618d66c8fe6168461621d37cd4c38f29add88379
daante12 <putrirahmadanti03@gmail.com>
2022-11-11 23:04:18 +0700

    task_management_app

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Container(margin: EdgeInsets.all(Get.height * 0.1),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: Colors.white,
+      ),
+      child: Row(children: [
+        Expanded(child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50), bottomLeft: Radius.circular(50),
+      ), 
+      color: Colors.blue,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('Welcome', style: TextStyle(color: Colors.white, fontSize: 70,
+          ),),
+          Text('Please Sign In', style: TextStyle(color: Colors.white, fontSize: 30,
+          ),),),
+      ),
+        Text('Start Journey With Us', style: TextStyle(color: Colors.white, fontSize: 20,
+        ),),), 
+      ],),
+      )
+      )
+      Expanded(child: Container(decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+              topRight: Radius.circular(50), bottomRight: Radius.circular(50),
+      ), 
+      ),
+      ))
+      ],),
+      ),
+    );
+  }
+}
