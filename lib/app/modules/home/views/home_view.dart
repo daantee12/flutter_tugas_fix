ec0f263daad465e886666bee90ad58dfb36a6fb2
daante12 <putrirahmadanti03@gmail.com>
2022-11-25 21:37:37 +0700

    commit tugas 5

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,127 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_tugas_fix/app/utils/widget/myTask.dart';
+import 'package:flutter_tugas_fix/app/utils/widget/myfriends.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:flutter_tugas_fix/app/utils/widget/header.dart';
+import 'package:flutter_tugas_fix/app/routes/app_pages.dart';
+import 'package:flutter_tugas_fix/app/utils/style/AppColors.dart';
+import 'package:flutter_tugas_fix/app/utils/widget/SideBar.dart';
+import 'package:flutter_tugas_fix/app/utils/widget/upcomingTask.dart';
+
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+   final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      key: _drawerKey,
+      drawer: const SideBar(),
+      backgroundColor: AppColors.primaryBg,
+     
+      body: SafeArea(
+        child: Row(
+          children: [
+         !context.isPhone?  Expanded(
+            flex: 2,
+            child:   const SideBar(),
+          ): const SizedBox(),
+          Expanded(
+            flex: 15,
+            child: Column(
+              children: [
+                !context.isPhone? const header(): 
+                Container(
+                  //content /isipage /screen
+                  padding: EdgeInsets.all(20),
+                child: Row(children: [IconButton(onPressed: (){
+                  _drawerKey.currentState!.openDrawer();
+                }, icon: Icon(Icons.menu, color: AppColors.primaryText,
+                ),
+  
+                   ),
+                   Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text('Task Management',style:TextStyle(fontSize: 20, color: AppColors.primaryText),
+          ),
+          Text('Manage task made easy with friends',style: TextStyle(fontSize: 14, color: AppColors.primaryText),
+          ),
+          ],
+          ),
+          Spacer(),
+         const Icon(Icons.notifications,color: Colors.grey,size: 30,
+         ),
+         const SizedBox(width: 15,
+         ),
+          ClipRRect(
+                              borderRadius: BorderRadius.circular(30),
+                              child: const CircleAvatar(
+                              backgroundColor: Colors.amber, 
+                              radius: 25, 
+                              foregroundImage: NetworkImage(
+                                    'https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/27/08/jennifer-lawrence.jpg?quality=75&width=982&height=726&auto=webp%27',
+         ),
+         ),
+         ),
+                ],
+                ),
+                ),
+            Expanded(child: Container(
+              padding: !context.isPhone? EdgeInsets.all(50): EdgeInsets.all(20),
+              margin: !context.isPhone? EdgeInsets.all(10): EdgeInsets.all(0),
+              decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: !context.isPhone? BorderRadius.circular(50): BorderRadius.circular(30)
+              ),
+              child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: [
+                     SizedBox(
+                       height: Get.height * 0.3,
+                       child: 
+                      
+                       Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         children: [
+                          Text(
+                            'My Task', 
+                            style: TextStyle(
+                              color: AppColors.primaryText, 
+                              fontSize: 30,
+                                     ),
+                                     ),
+                                          SizedBox(
+                                            height: 20,
+                                            
+                                          ),
+              //mystask
+              MyTask(),
+            
+                         ],
+                       ),
+                     ),
+          !context.isPhone? Expanded(
+               child: Row(
+                 
+                 children: [
+                   UpcomingTask(),
+                   MyFriends(),
+                   
+                     
+                 ],    
+                ),
+             ): UpcomingTask(),
+              ]),
+            ),
+            ),
+            
+            ]),
+          ),
+        ],
         ),
       ),
     );
--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_tugas_fix/app/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 import '../controllers/login_controller.dart';
@@ -10,54 +10,110 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[100],
-      body: Container(margin: EdgeInsets.all(Get.height * 0.1),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(50),
-        color: Colors.white,
+      body: Container(
+        margin: context.isPhone
+        ? EdgeInsets.all(Get.width * 0.1)
+        : EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
       ),
-      child: Row(children: [
-        Expanded(child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(50), bottomLeft: Radius.circular(50),
+        child: Row(children: [
+          // biru
+          !context.isPhone?
+          Expanded(
+            child: Container(
+              decoration : const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                bottomLeft: Radius.circular(50),
+                ),
+                color: Colors.blue,
+     ),
+     child: Padding(
+      padding: const EdgeInsets.all(30.0),
+     child: Column(
+     crossAxisAlignment : CrossAxisAlignment.start,
+     mainAxisAlignment : MainAxisAlignment.center,
+     children: const [
+      Text(
+        'Welcome', 
+      style: TextStyle(
+      color: Colors.white,
+      fontSize: 70, 
+      ),     
       ), 
-      color: Colors.blue,
+      Text(
+      'Please Sign In',
+      style: TextStyle(
+      color: Colors.white,
+      fontSize: 30, 
+      ),  
+     ),
+      Text(
+        'Start Journey With Us',
+      style: TextStyle(
+      color: Colors.white,
+      fontSize: 20, 
+      ),  
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Welcome', style: TextStyle(color: Colors.white, fontSize: 70,
-          ),),
-          Text('Please Sign In', style: TextStyle(color: Colors.white, fontSize: 30,
-          ),
-          ),
-          ),
+     ]),
+    ),
+    ),
+          )
+          :const SizedBox(),
+  // putih
+    Expanded(
+      child: Container(
+      decoration : const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                topRight: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+                ),
+     ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        context.isPhone?
+        Column(
+     // crossAxisAlignment : CrossAxisAlignment.start,
+     mainAxisAlignment : MainAxisAlignment.center,
+     children: const [
+      Text(
+        'Welcome', 
+      style: TextStyle(
+      color: Colors.grey,
+      fontSize: 40, 
+      ),     
+      ), 
+      Text(
+      'Please Sign In',
+      style: TextStyle(
+      color: Colors.grey,
+      fontSize: 20, 
+      ),  
+     ),
+      Text(
+        'Start Journey With Us',
+      style: TextStyle(
+      color: Colors.grey,
+      fontSize: 15, 
+      ),  
       ),
-        Text('Start Journey With Us', style: TextStyle(
-          color: Colors.white, 
-        fontSize: 20,
-        ), 
-        ), 
-      ]),
+     ])
+     : const SizedBox(),
+    Image.asset(
+      'assets/images/login.png',
+    height: Get.height * 0.5, 
+    ),
+    FloatingActionButton.extended(
+      onPressed: () => Get.toNamed(Routes.HOME), 
+      label: const Text('Sign In With Google'),
       ),
-        Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-              topRight: Radius.circular(50), 
-              bottomRight: Radius.circular(50),
-              ), 
-          ),
-          child: Column(children: [
-            Image.asset('assets/images/login.png'),
-          FloatingActionButton.extended(
-            onPressed: (){}, label: text('Sign In With Google'), Icon(icon), )
-          ]),
+            ]),
+          ))
+        ]),
       ),
-      ),
-      ]),
+    );
   }
 }
